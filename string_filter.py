83df1f66a4683fffedde81ed52eda14e04bb3cae
Bhoopendra Chaurasia <bchaurasia97@gmail.com>
2022-09-08 23:00:33 +0530

    String filter

---
+++ string_filter.py
@@ -0,0 +1,4 @@
+str='Bh234565oo00pe56@#$n^d*&ra';
+
+for i in range(len(str)):
+    print(i)
--- string_function.py
+++ string_function.py
@@ -1,5 +1,25 @@
 str='Bhoopendra Chaurasia living in Indore'
 print()
+print('Original string is:',str)
+print()
 print('Convert in lower case:',str.lower())
 print()
 print('Convert in upper case:',str.upper())
+print()
+print('Convert in title case:',str.title()) #in title case first character convert in upper case
+print()
+print('Convert first character in Capital letter of string:',str.capitalize())
+print()
+print('Convert caseless string:',str.casefold()) # casefold is convert in lower case means caseless
+print()
+print('Count total occurance of specified characters in string:',str.count('i'))
+print()
+print('End with specified characters it return true or false:',str.endswith('e'))
+print()
+print('End with specified characters it return true or false:',str.endswith('i'))
+print()
+print('Find character index of word characters index in string:', str.find('i'))
+print()
+str1='I am a software engineer';
+str2="I am living in Indore";
+print('Join two string:',str2.join('Bh00'))
